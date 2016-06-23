46bb6c4cfcbe65e9efeb3ae7d30542164047415d
Lee Witek <neomech@gmail.com>
2016-06-23 13:14:05 +0100

    added module touch

---
+++ moduletouch.cpp
@@ -0,0 +1,22 @@
+#include "moduletouch.h"
+
+void ModuleTouch::Init(int chan) {
+	SetChannel(chan);
+	m_prevState = 255;  // initially set to impossible state so we get at least 1 update at start
+}
+
+void ModuleTouch::Update(Stream* stream) {
+	uint8_t newState = GetState();
+
+	if (newState != m_prevState) {
+		m_prevState = newState;
+		stream->print("u ");
+		stream->print(Channel());
+		stream->print("/");
+		stream->print(Name());
+		stream->print((m_prevState & 1) ? " 1," : " 0,");
+		stream->print((m_prevState & 2) ? "1," : "0,");
+		stream->print((m_prevState & 4) ? "1," : "0,");
+		stream->print((m_prevState & 8) ? "1\r\n" : "0\r\n");
+	}
+}
---
+++ moduletouch.h
@@ -0,0 +1,24 @@
+#ifndef ARDUINO_FLOTILLA_MODULETOUCH_H
+#define ARDUINO_FLOTILLA_MODULETOUCH_H
+
+#include <Arduino.h>
+#include "module.h"
+
+class ModuleTouch : public Module {
+  private:
+	uint8_t m_prevState;
+
+  protected:
+	virtual uint8_t GetState() = 0;
+
+  public:
+	void Init(int chan);
+
+	const char* Name() {
+		return "touch";
+	}
+
+	virtual void Update(Stream* stream);
+};
+
+#endif
