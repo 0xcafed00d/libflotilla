586ec2066753aaeeb2143f8e7f60eebff2718ef9
Lee Witek <neomech@gmail.com>
2016-07-01 23:27:45 +0100

    use averaged values

--- modulemotion.cpp
+++ modulemotion.cpp
@@ -21,11 +21,17 @@ void ModuleMotion::Update(Stream* stream) {
 	m_accel.x.addValue(accel.x);
 	m_accel.y.addValue(accel.y);
 	m_accel.z.addValue(accel.z);
+	m_mag.x.addValue(accel.x);
+	m_mag.y.addValue(accel.y);
+	m_mag.z.addValue(accel.z);
 
 	if (m_timeout.hasTimedOut()) {
 		accel.x = m_accel.x.value();
 		accel.y = m_accel.y.value();
 		accel.z = m_accel.z.value();
+		mag.x = m_mag.x.value();
+		mag.y = m_mag.y.value();
+		mag.z = m_mag.z.value();
 
 		if (accel != m_prevAccel || mag != m_prevMag) {
 			printUpdateHeader(stream);
@@ -41,5 +47,8 @@ void ModuleMotion::Update(Stream* stream) {
 		m_accel.x.reset();
 		m_accel.y.reset();
 		m_accel.z.reset();
+		m_mag.x.reset();
+		m_mag.y.reset();
+		m_mag.z.reset();
 	}
 }
